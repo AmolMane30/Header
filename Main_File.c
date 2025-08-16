f0d8ce4dda77d309dfc0a8c3798bc51c963a6b86
AmolMane30 <maneamol3001@gmail.com>
2025-08-16 14:56:44 +0530

    Update Main_File.c

--- Main_File.c
+++ Main_File.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
 
+// This is user defined header which is present inside another .h file
+#include "My_Header"
+
+int main()
+{
+  int No = 21;
+  int i = DOZEN;
+  float f = 51.98f;
+  char ch = 'A';
+
+  struct Demo dobj;
+  dobj.X = No;
+  dobj.Y = PI;
+  dobj.Z = 'T';
+
+  printf("Jay Ganesh...\n");
+  printf("No: %d\n",No);
+  printf("f : %f\n",f);
+  printf("ch : %c\n",ch);
+  printf("i : %d\n",i);
+
+  printf("X : %d\n",dobj.X);
+  printf("Y : %f\n",dobj.Y);
+  printf("Z : %c\n",dobj.Z);
+
+  printf("Size of Structure is : %d bytes.\n",sizeof(dobj));
+
+  return 0;
+}
