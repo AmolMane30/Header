0656e1b2de874a86b3e7b15511f8093c723afd43
AmolMane30 <maneamol3001@gmail.com>
2025-08-16 15:11:18 +0530

    Update My_Header.h

--- My_Header.h
+++ My_Header.h
@@ -1,3 +1,14 @@
+//-----------------------------------------------------------------------------------------------------------------------------------//
+//        Project Title: User_Defined_Header_File
+//
+//        Description  : -> Attached "My_Header.h" file which is a user defined header file provides function prototypes, 
+//                          variable declarations, structure and union declarations, user defined MACROS.
+//                       -> Preprocessor includes these files by checking the .h file in current directory and gives the 
+//                          necessary things as needed by the (Main_File.c) program.
+//
+//        Author       : Amol R. Mane
+//        Date         : 16 August 2025
+//-----------------------------------------------------------------------------------------------------------------------------------//
 #define DOZEN 12
 #define PI 3.14
 
